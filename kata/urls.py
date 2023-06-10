2d9a2532325fc56c73532cb168da35089d3573b0
Jeffrey <TheCrablander@gmail.com>
2023-06-10 17:07:17 -0500

    single model hooked up to django rest framework added for testing purposes, and to have suitable baseline for exercises

--- kata/urls.py
+++ kata/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from myapp import views as myapp_views 
+
+
+router = routers.DefaultRouter()
+router.register(r'people', myapp_views.PersonViewset)
+
 
 urlpatterns = [
+    path('api/', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
